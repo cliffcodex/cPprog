f4419b5ae9764af1fa4169d17aad0cc11a5ec450
cliffcodex <usaimbishr@icloud.com>
2025-05-11 23:08:43 -0700

    Update native-lib.cpp

--- native-lib.cpp
+++ native-lib.cpp
@@ -1,19 +1,10 @@
-
 #include <jni.h>
+#include <string>
 
-extern "C"
-JNIEXPORT jdouble JNICALL
-Java_com_west_calculator_Calculator_calculate(
-        JNIEnv *env,
-        jobject /* this */,
-        jdouble a,
-        jdouble b,
-        jint operatorCode) {
-    switch (operatorCode) {
-        case 0: return a + b;
-        case 1: return a - b;
-        case 2: return a * b;
-        case 3: return b != 0 ? a / b : 0;
-        default: return 0;
-    }
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_west_*_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
 }
