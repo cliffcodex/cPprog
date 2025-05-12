a5996217a0ea5777e94c0d478d2d37a2e927e926
cliffcodex <usaimbishr@icloud.com>
2025-05-11 23:14:55 -0700

    Update Theme.kt

--- Theme.kt
+++ Theme.kt
@@ -1,1 +1,32 @@
-// Standard Compose Theme file
+package com.west.%.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColors = darkColorScheme(
+    primary = Color(0xFF1EB980),
+    secondary = Color(0xFF045D56)
+)
+
+private val LightColors = lightColorScheme(
+    primary = Color(0xFF00695C),
+    secondary = Color(0xFF004D40)
+)
+
+@Composable
+fun %(
+    darkTheme: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColors else LightColors
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography(),
+        content = content
+    )
+}
