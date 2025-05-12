dc8b6fded133f1790d195b6f493bc2fc5539f382
cliffcodex <usaimbishr@icloud.com>
2025-05-11 23:11:37 -0700

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,4 +1,4 @@
-package com.west
+package com.west.@
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -45,7 +45,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.west.cstack.ui.theme.CStackTheme
+import com.west.@.ui.theme.CStackTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -53,7 +53,7 @@ class MainActivity : ComponentActivity() {
 
     companion object {
         init {
-            System.loadLibrary("cstack")
+            System.loadLibrary("@")
         }
     }
 
